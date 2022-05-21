facf62ea3de70fc81ac9e90980e0a4dc5423ee17
Utkarsh Raj <utkarsh.raj@outlook.com>
2022-05-21 13:52:45 +0530

    rolling back the mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,24 +3,9 @@ import { MongooseModule, MongooseModuleAsyncOptions } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from './config/config.module';
-import { ConfigService } from './config/config.service';
-import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => (
-        {
-          uri: configService.get('DB_URL'),
-          useNewUrlParser: true,
-          useUnifiedTopology: true,
-        } as MongooseModuleAsyncOptions
-      )
-    }),
-    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
