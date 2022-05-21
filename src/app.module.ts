dc02748af13562fa22cbe4beed882f755d4ef2ad
Utkarsh Raj <utkarsh.raj@outlook.com>
2022-05-21 13:17:36 +0530

    mongodb initialized and users module created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule, MongooseModuleAsyncOptions } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from './config/config.module';
+import { ConfigService } from './config/config.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => (
+        {
+          uri: configService.get('DB_URL'),
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        } as MongooseModuleAsyncOptions
+      )
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
