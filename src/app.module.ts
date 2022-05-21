ffbe2b624934ea130fa5946788316fc835bcaf00
Utkarsh Raj <utkarsh.raj@outlook.com>
2022-05-22 00:37:04 +0530

    mongoose module initialized and technologies finalized

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,24 +5,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from './config/config.module';
 import { ConfigService } from './config/config.service';
-import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => (
-        {
+      useFactory: (configService: ConfigService) =>
+        ({
           uri: configService.get('DB_URL'),
           useNewUrlParser: true,
           useUnifiedTopology: true,
-        } as MongooseModuleAsyncOptions
-      )
+        } as MongooseModuleAsyncOptions),
     }),
-    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -2,22 +2,22 @@ import { parse } from 'dotenv';
 import * as fs from 'fs';
 
 export interface EnvConfig {
-    [key: string]: string;
+  [key: string]: string;
 }
 
 export class ConfigService {
-    private readonly envConfig: EnvConfig;
+  private readonly envConfig: EnvConfig;
 
-    constructor(filepath: string) {
-        const config = parse(fs.readFileSync(filepath));
-        this.envConfig = { ...config };
-    }
+  constructor(filepath: string) {
+    const config = parse(fs.readFileSync(filepath));
+    this.envConfig = { ...config };
+  }
 
-    get(key: string): string {
-        return this.envConfig[key];
-    }
+  get(key: string): string {
+    return this.envConfig[key];
+  }
 
-    isEnv(env: string): boolean {
-        return this.envConfig.APP_ENV === env;
-    }
+  isEnv(env: string): boolean {
+    return this.envConfig.APP_ENV === env;
+  }
 }
