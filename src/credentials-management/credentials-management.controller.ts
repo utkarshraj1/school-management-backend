bf7d44744a8ed51c29370332ae1a44ccdaa4ef74
Utkarsh Raj <utkarsh.raj@outlook.com>
2022-06-17 01:08:23 +0530

    feature: Controller methods created

--- src/credentials-management/credentials-management.controller.ts
+++ src/credentials-management/credentials-management.controller.ts
@@ -1,11 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CredentialsManagementService } from './credentials-management.service';
 
-@Controller('credentials-management')
+@Controller()
 export class CredentialsManagementController {
   constructor(
     private readonly credentialsManagementService: CredentialsManagementService,
   ) {}
 
   // APIs will go here
+  @Post('signIn')
+  signIn(@Body() body: any): string {
+    return 'Successfully called signIn method';
+  }
+
+  @Post('signUp')
+  signUp(@Body() body: any): string {
+    return 'Successfully called the signUp method';
+  }
 }
