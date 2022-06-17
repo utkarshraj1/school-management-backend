b72d6fecd379522e3db6014caf86a9685f295c07
Utkarsh Raj <utkarsh.raj@outlook.com>
2022-06-17 15:08:09 +0530

    feature: sign in api added

--- src/credentials-management/credentials-management.controller.ts
+++ src/credentials-management/credentials-management.controller.ts
@@ -1,35 +1,40 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
 import { CredentialsManagementService } from './credentials-management.service';
 
-@Controller()
+@Controller('/credentials')
 export class CredentialsManagementController {
   constructor(
     private readonly credentialsManagementService: CredentialsManagementService,
   ) {}
 
-  // APIs will go here
-  // @Post('signIn')
-  // signIn(@Body() body: any): string {
-  //   return 'Successfully called signIn method';
-  // }
+  /**
+   * Sign in with credentials
+   * @param userCredentials An object with username and password
+   * @returns An appropriate object inside a promise
+   */
+  @Post('/signIn')
+  async signIn(@Body() userCredentials: any): Promise<any> {
+    return this.credentialsManagementService.signInUser(userCredentials);
+  }
 
   /**
    * Adding new user
    * @param credentials An object with username, password and user_reg_id
    * @returns A promise with success message
    */
-  @Post('signUp')
+  @Post('/signUp')
   async signUp(@Body() credentials: any): Promise<any> {
     if (
       !credentials.username ||
       !credentials.user_reg_id ||
       !credentials.password
     ) {
-      // User proper error handling
-      return { status: 404, message: 'All required fields must be entered!' };
+      throw new BadRequestException({
+        status: 400,
+        message: 'All the required fields must be sent.',
+        ok: false,
+      });
     }
-    const response =
-      await this.credentialsManagementService.registerCredentials(credentials);
-    return response;
+    return this.credentialsManagementService.registerCredentials(credentials);
   }
 }
--- src/credentials-management/credentials-management.service.ts
+++ src/credentials-management/credentials-management.service.ts
@@ -1,4 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import {
+  HttpException,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import * as password from 'password-hash-and-salt';
@@ -15,22 +21,44 @@ export class CredentialsManagementService {
   ) {}
 
   /**
-   * Transforms the incoming password into a new hashed and salted password
+   * Encrypts/Decrypts the password
    * @param pwd A password string
-   * @returns A hashed password inside of a promise
+   * @param action The action to be taken - Encrypt/Decrypt
+   * @returns A promise with hashed password (incase of encryption) or a success message (incase of decryption)
    */
-  transformPassword(pwd: string): Promise<any> {
-    return new Promise((resolve, reject) => {
-      password(pwd).hash((error, hash) => {
-        if (error) {
-          reject(
-            'Something is not right in transformPassword method, please check',
-          );
-        } else {
-          resolve(hash);
-        }
-      });
-    });
+  transformPassword(pwd: string, action: string): Promise<any> {
+    switch (action) {
+      case 'Encrypt':
+        return new Promise((resolve, reject) => {
+          password(pwd).hash((error, hash) => {
+            if (error) {
+              console.log(
+                'Error came in transformPassword method while encrypting.',
+              );
+              reject('');
+            } else {
+              resolve(hash);
+            }
+          });
+        });
+      case 'Decrypt':
+        return new Promise((resolve, reject) => {
+          const [hash, enteredPwd] = pwd.split('<divide>');
+          password(enteredPwd).verifyAgainst(hash, (err, verified) => {
+            if (err) {
+              console.log(
+                'Error came in transformPassword method while decrypting.',
+              );
+              reject('');
+            }
+            if (!verified) {
+              resolve(false);
+            } else {
+              resolve(true);
+            }
+          });
+        });
+    }
   }
 
   /**
@@ -49,6 +77,10 @@ export class CredentialsManagementService {
     return credential;
   }
 
+  async createJWTToken(userCred: any): Promise<any> {
+    // Impementation in progress
+  }
+
   /**
    * Register new users
    * @param credentials A credential object with username, user_reg_id and password
@@ -57,19 +89,65 @@ export class CredentialsManagementService {
   async registerCredentials(credentials: any): Promise<any> {
     const userCred = await this.findCredential(credentials.username);
     if (userCred !== '') {
-      // Use proper error handling
-      return { status: 404, message: 'User is already present' };
+      throw new HttpException(
+        {
+          status: 200,
+          ok: false,
+          message: 'User credentials are already present.',
+        },
+        200,
+      );
     }
-    const transformedPwd = await this.transformPassword(credentials.password);
+    const transformedPwd = await this.transformPassword(
+      credentials.password,
+      'Encrypt',
+    );
     const response = await this.userCredentialsModel.create({
       ...credentials,
       password: transformedPwd,
       active_status: true,
     });
     if (!response._id) {
-      // use proper error handling
-      return { status: 404, message: 'Something went wrong...' };
+      throw new InternalServerErrorException({
+        status: 500,
+        message: 'Something went wrong while inserting user credentials in Db.',
+        ok: false,
+      });
     }
-    return { status: 200, message: 'Successfully added the new user...' };
+    return {
+      status: 200,
+      message: 'User credentials inserted successfully!',
+      ok: true,
+    };
+  }
+
+  /**
+   * Sign in user with credentials
+   * @param userCred An object with username and password
+   * @returns An object with appropriate message/data inside a promise
+   */
+  async signInUser(userCred: any): Promise<any> {
+    const credObjFromDb = await this.findCredential(userCred.username);
+    if (credObjFromDb === '') {
+      throw new NotFoundException({
+        status: 404,
+        ok: false,
+        message: 'User does not exists.',
+      });
+    }
+    const isUserAuthentic = await this.transformPassword(
+      credObjFromDb.password + '<divide>' + userCred.password,
+      'Decrypt',
+    );
+    if (!isUserAuthentic) {
+      throw new UnauthorizedException({
+        status: 401,
+        message:
+          'User is either entering wrong password or unauthorized! Please connect with the administrator',
+        ok: false,
+      });
+    }
+    // to return a JWT token
+    return { status: 200, message: 'User is authorized', ok: true };
   }
 }
